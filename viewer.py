6dd18ee16b83b63e64550bef8dd332d5a49d7483
darren wang <dachwadachwa@gmail.com>
2021-03-01 17:25:58 -0800

    fixes to viewer class for test

--- viewer.py
+++ viewer.py
@@ -5,21 +5,27 @@ import numpy as np
 # blueprint for video feed viewing service
 class Viewer:
     def __init__(self, addr: str, port: str):
-        # bind to addr to receive incoming image feed
+        # connect to target addr:port to receive incoming video feed
+        sock = "tcp://" + addr + ":" + port
         context = zmq.Context()
         self.socket = context.socket(zmq.SUB)
-        self.socket.bind("tcp://" + addr + ":" + port)
+        self.socket.connect(sock)
         self.socket.setsockopt_string(zmq.SUBSCRIBE, np.unicode(""))
+
+        # start the viewer
         self.start()
 
     def start(self) -> None:
         # display image feed
         while True:
             try:
+                # receive string from server, convert to image
                 img_str = self.socket.recv_string()
                 buff = base64.b64decode(img_str)
-                image = np.fromstring(image, dtype=np.uint8)
+                image = np.fromstring(buff, dtype=np.uint8)
                 source = cv2.imdecode(image, 1)
+
+                # display image
                 cv2.imshow("Cat Tracker", source)
                 cv2.waitKey(1)
 
@@ -35,6 +41,7 @@ if __name__ == "__main__":
     parser.add_argument('-p', '--port', help='server port')
     args = parser.parse_args()
 
+    # start client process for viewing video stream
     print ("Starting viewer...")
     view = Viewer(args.addr, args.port)
     print ("Shutting down...")
